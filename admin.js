da3ccc79e58a64753f75517026c618d343d5f13c
ZulayNicolOrtegónPintoADSO <zulyortegon15@gmail.com>
2023-03-14 22:07:23 -0500

    🚀feat: avance para listar

--- admin.js
+++ admin.js
@@ -1,30 +1,55 @@
-let nom =document.getElementById("nombre");
+let nombre =document.getElementById("nombre");
 let nota = document.getElementById("nota");
 
 let consejo = document.getElementById("con");
 
+let list =[]
+let cant = 0
+
 let cal = document.getElementById("cal");
 cal.addEventListener("click" , (e)=>{
+
+    let nom = nombre.value
     let not = parseFloat(nota.value)
-    if(not >= 90 && not <= 100){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra A"
+    console.log(nom , not)
+
+    if (nom !== "" & not >= 0) {
+        cant += 1
+        list.push(cant)
+        list.push(nom)
+        list.push(not)
+        alert(list)
+        consejo.innerText= list
     }
-    else if(not >= 80 && not <= 89){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra B"
-    }
-    else if(not >= 70 && not <= 79){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra C"
-    }
-    else if(not >= 60 && not <= 69){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra D"
-    }
-    else if(not < 60 && not >0){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra E"
-    }
-    else if(not == 0){
-        consejo.innerText= nom.value + "\n Tú calificación es la letra Z"
-    }
+
     else{
-        consejo.innerText="El puntaje indicado no está en el rango de calificaciones, por favor, vuelve a intentarlo"
+        alert("Por favor completa ambos campos: Nombre y Nota")
     }
+
+    // let not = parseFloat(nota.value)
+    // if(not >= 90 && not <= 100){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra A"
+    // }
+    // else if(not >= 80 && not <= 89){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra B"
+    // }
+    // else if(not >= 70 && not <= 79){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra C"
+    // }
+    // else if(not >= 60 && not <= 69){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra D"
+    // }
+    // else if(not < 60 && not >0){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra E"
+    // }
+    // else if(not == 0){
+    //     consejo.innerText= nom.value + "\n Tú calificación es la letra Z"
+    // }
+    // else{
+    //     consejo.innerText="El puntaje indicado no está en el rango de calificaciones, por favor, vuelve a intentarlo"
+    // }
+
+
+
 })
+
